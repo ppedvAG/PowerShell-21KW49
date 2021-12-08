2ffaac52c1d177988d3966e5ac53a1e2ad007797
Stefan Ober <StefanO@ppedv.de>
2021-12-08 15:48:02 +0100

    Skriptbeispiel: Simple mit Parametern

---
+++ FortgeschritteneTechniken/Skriptbeispiel/Get-LogonEvent.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+   kurze Beschreibung: Abfrage von Anmeldespezifischen Events
+.DESCRIPTION
+   Mit diesem Skript können Anmeldebezogene Events abegrafragt werden wie zb Anmelde, Abmeldeevents sowie fehlgeschlagene Anmeldungen.
+.PARAMETER EventId
+   Mit diesem Parameter wird die Art des Events festgelegt. Mögliche Werte sind:
+   4624 | Anmeldung
+   4625 | fehlgeschlagene Anmeldung
+   4634 | Abmeldung
+.EXAMPLE
+   Get-LogonEvent.ps1 -EventId 4625
+
+   Index Time          EntryType   Source                 InstanceID Message
+   ----- ----          ---------   ------                 ---------- -------
+   21982 Dez 08 10:10  FailureA... Microsoft-Windows...         4625 Fehler beim Anmelden eines Kontos....
+   17483 Dez 07 15:04  FailureA... Microsoft-Windows...         4625 Fehler beim Anmelden eines Kontos....
+   17482 Dez 07 15:04  FailureA... Microsoft-Windows...         4625 Fehler beim Anmelden eines Kontos....
+   14425 Dez 07 09:15  FailureA... Microsoft-Windows...         4625 Fehler beim Anmelden eines Kontos....
+   14424 Dez 07 09:15  FailureA... Microsoft-Windows...         4625 Fehler beim Anmelden eines Kontos....
+
+   Weiter Erklärungstext
+.EXAMPLE
+   Ein weiteres Beispiel für die Verwendung dieses Cmdlets
+#>
+[cmdletBinding()]
+Param(
+[Parameter(Mandatory=$true)]
+[int]$EventId,
+
+[int]$Newest = 5,
+
+[string]$ComputerName = "localhost"
+)
+
+Write-Verbose -Message "Zusätzliche Ausgabe: Der User hat das Skript mit folgenden Werten gestartet:"
+Write-Verbose -Message "$EventId | $ComputerName | $Newest"
+
+Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object -FilterScript {$PSItem.EventId -eq $EventId} | Select-Object -First $Newest
+
