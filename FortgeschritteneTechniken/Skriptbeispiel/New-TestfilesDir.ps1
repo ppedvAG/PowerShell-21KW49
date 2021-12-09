5e9cc2af57f146e61dea5af38b3053ae68500a39
Stefan Ober <StefanO@ppedv.de>
2021-12-09 10:00:31 +0100

    Skript angepasst / vereinfacht mit Funktionen

--- FortgeschritteneTechniken/Skriptbeispiel/New-TestfilesDir.ps1
+++ FortgeschritteneTechniken/Skriptbeispiel/New-TestfilesDir.ps1
@@ -1,6 +1,7 @@
 ﻿[cmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
+    [ValidateScript({Test-Path -Path $PSItem})]
     [string]$Destinationpath,
 
     [ValidateRange(1,10)]
@@ -14,6 +15,30 @@ param(
 
     [switch]$Force
 )
+#Functionsdeklaration
+function New-TestFiles
+{
+[cmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateRange(1,10)]
+    [int]$FileCount,
+
+    [Validatelength(2,20)]
+    [string]$FileName = "Datei",
+
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({Test-Path -Path $PSItem})]
+    [string]$Destinationpath
+)
+
+    for($i = 1; $i -le $FileCount; $i++)
+    {
+        $Filename = $FileName + ("{0:D2}" -f $i) + ".txt"
+        New-Item -Path $Destinationpath -Name $Filename -ItemType File
+    }
+}
+
 
 $TestFilesDir = $Destinationpath + "\" + $DirName
 
@@ -37,20 +62,29 @@ else
     New-Item -Path $TestFilesDir -ItemType Directory
 }
 
+Write-Debug -Message "Vor Dateierstellung Rootverzeichnis"
+
+New-TestFiles -FileCount $FileCount -FileName "Datei" -Destinationpath $TestFilesDir
+
+<# Wurde ersetzt durch die Funktion New-TestFiles
 for($i = 1; $i -le $FileCount; $i++)
 {
     $Filename = "Datei" + ("{0:D2}" -f $i) + ".txt"
     New-Item -Path $TestFilesDir -Name $Filename -ItemType File
 }
+#>
 
 for($i = 1; $i -le $DirCount; $i++)
 {
     $DirName = "Ordner" + ("{0:D2}" -f $i)
     $subdir = New-Item -Path $TestFilesDir -Name $DirName -ItemType Directory
 
+    New-TestFiles -Destinationpath $subdir.FullName -FileName ("O$i" + "Datei") -FileCount $FileCount
+    <# Wurde ersetzt durch die Funktion New-TestFiles
     for($j = 1; $j -le $FileCount; $j++)
     {
         $Filename = "O" + $i + "Datei" + ("{0:D2}" -f $j) + ".txt"
         New-Item -Path $subdir.FullName -Name $Filename -ItemType File
     }
+    #>
 }
